b7e1302cb5da1613c390101a0e4bbe2d1544d4f8
Ling-ex <sahrulgunawangugun@gmail.com>
2023-08-13 21:50:42 +0700

    ADD FAKE QUOTLY

--- ProjectDark/modules/quotly.py
+++ ProjectDark/modules/quotly.py
@@ -2,12 +2,15 @@
 # Kang by DarkPyro - 2023
 
 import asyncio
+import requests
 
-from pyrogram import Client, filters
+from base64 import b64decode
+from pyrogram import Client, filters, errors
 from pyrogram.types import Message
 
 from config import CMD_HANDLER as cmd
 from ProjectDark.helpers.tools import get_arg
+from ProjectDark.helpers.copas import render_message, resize_image
 
 from .help import add_command_help
 
@@ -39,6 +42,77 @@ async def quotly(client: Client, message: Message):
                 )
             else:
                 return await message.edit("**ERRO!**")
+
+
+async def fake_quote_cmd(client: Client, message: Message):
+    send_for_me = "!me" in message.command or "!ls" in message.command
+
+    if len(message.command) < 3:
+        return await message.edit(f"{message.text} <username> <pesan>")
+
+    target_user = message.command[1]
+    if not target_user.startswith("@"):
+        return await message.edit("format username salah")
+    target_user = target_user[1:]
+
+    try:
+        user = await client.get_users(target_user)
+    except errors.exceptions.bad_request_400.UsernameNotOccupied:
+        return await message.edit("username tidak ditemukan")
+    except IndexError:
+        return await message.edit("jangan gunakan username CH/GROUP")
+
+    if user.id in DEVS:
+        return await message.reply("LOL!!")
+
+    fake_quote_text = " ".join(message.command[2:])
+
+    if not fake_quote_text:
+        return await message.edit("Pesan kosong")
+
+    q_message = await client.get_messages(message.chat.id, message.id)
+    q_message.text = fake_quote_text
+    q_message.entities = None
+
+    q_message.from_user.id = user.id
+    q_message.from_user.first_name = user.first_name
+    q_message.from_user.last_name = user.last_name
+    q_message.from_user.username = user.username
+    q_message.from_user.photo = user.photo
+
+    if send_for_me:
+        await message.delete()
+        message = await client.send_message("me", "Memproses...")
+    else:
+        await message.edit("Loading...")
+
+    url = "https://quotes.fl1yd.su/generate"
+    user_auth_1 = b64decode("Y2llIG1hbyBueW9sb25nIGNpaWUuLi4uLCBjb2xvbmcgYWphIGJhbmcgamFkaWluIHByZW0gdHJ1cyBqdWFsLCBrYWxpIGFqYSBiZXJrYWggaWR1cCBsdS4uLi4=")
+    params = {
+        "messages": [await render_message(client, q_message)],
+        "quote_color": "#162330",
+        "text_color": "#fff",
+    }
+
+    response = requests.post(url, json=params)
+    if not response.ok:
+        return await message.edit(
+            f"<b>GAGAL!</b>\n" f"<code>{response.text}</code>"
+        )
+
+    resized = resize_image(
+        BytesIO(response.content), img_type="webp"
+    )
+    await message.edit("mengirim sticker...")
+
+    try:
+        func = client.send_sticker
+        chat_id = "me" if send_for_me else message.chat.id
+        await func(chat_id, resized)
+    except errors.RPCError as e:
+        await message.edit(e)
+    else:
+        await message.delete()
 
 
 add_command_help(
